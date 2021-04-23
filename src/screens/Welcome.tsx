9c0c13be79a36fc079f8917bec780108c70f5e91
gislainejessica <gislainehjessica@gmail.com>
2021-04-23 02:53:36 -0300

    welcome screen ok

--- src/screens/Welcome.tsx
+++ src/screens/Welcome.tsx
@@ -1,45 +1,48 @@
-import React from 'react';
+import React from 'react'
 import {
   StyleSheet,
   Text,
   SafeAreaView,
   Image,
   TouchableOpacity,
-  Dimensions
+  Dimensions,
+  View
 } from 'react-native';
 
-import wateringimg from "../../assets/watering.png"
-import colors from '../styles/colors';
+import { Entypo } from '@expo/vector-icons'
+
+import wateringimg from '../../assets/watering.png'
+
+import colors from '../styles/colors'
+import fonts from '../styles/fonts'
 
 export function Welcome() {
   return (
     <SafeAreaView style={styles.container}>
-
-      <Text style={styles.title}>
-        Gerencie {'\n'}
-        suas plantas {'\n'}
-        de forma  fácil
-      </Text>
-
-      <Image
-        source={wateringimg}
-        style={styles.image}
-        resizeMode="contain"
-      />
-
-      <Text style={styles.subTitle}>
-        Não esqueça mais de regar suas plantas. Nós cuidamos de lembrar você
-        sempre que precisar.
-      </Text>
-
-      <TouchableOpacity
-        style={styles.button}
-        activeOpacity={0.7}>
-        <Text style={styles.textButton}>
-          >
+      <View style={styles.wrapper}>
+        <Text style={styles.title}>
+          Gerencie {'\n'}
+          suas plantas de {'\n'}
+          forma  fácil
         </Text>
-      </TouchableOpacity>
 
+        <Image
+          source={wateringimg}
+          style={styles.image}
+          resizeMode="contain"
+        />
+
+        <Text style={styles.subTitle}>
+          Não esqueça mais de regar suas plantas. Nós cuidamos de lembrar você
+          sempre que precisar.
+        </Text>
+
+        <TouchableOpacity
+          style={styles.button}
+          activeOpacity={0.7}>
+          <Entypo name="chevron-right" style={styles.textIcon} />
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 }
@@ -47,26 +50,31 @@ export function Welcome() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  wrapper: {
+    flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'space-around',
+    paddingHorizontal: 20,
   },
   title: {
-    fontSize: 32,
-    fontWeight: 'bold',
+    fontSize: 28,
     textAlign: 'center',
     color: colors.heading,
-    marginTop: 38
+    marginTop: 38,
+    fontFamily: fonts.heading,
+    lineHeight: 34,
   },
   image: {
     height: Dimensions.get('window').width * 0.7,
-    // width: 292
   },
   subTitle: {
     textAlign: 'center',
     fontSize: 18,
     paddingHorizontal: 20,
     color: colors.heading,
+    fontFamily: fonts.text,
   },
   button: {
     backgroundColor: colors.green,
@@ -75,10 +83,10 @@ const styles = StyleSheet.create({
     borderRadius: 16,
     marginBottom: 10,
     width: 56,
-    height: 56
+    height: 56,
   },
-  textButton: {
+  textIcon: {
     color: colors.white,
-    fontSize: 24,
+    fontSize: 32,
   },
 });
